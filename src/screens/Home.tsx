fd4cab45439c6075f23a04edb24f59af553e9c97
Ana Clara Mendes Bull <anaclaramendesbull@gmail.com>
2022-07-21 02:01:25 -0300

    up

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,22 @@
-import { HStack, VStack, IconButton, useTheme, Text, Heading } from 'native-base';
-import { SignOut } from 'phosphor-react-native';
+import { HStack, VStack, IconButton, useTheme, Text, Heading, FlatList, Center } from 'native-base';
+import { ChatTeardropText, SignOut } from 'phosphor-react-native';
+import { useState } from 'react';
 import Logo from '../assets/logo_secondary.svg'
+import { Button } from '../components/Button';
 import { Filter } from '../components/Filter';
+import { Order, OrderProps } from '../components/Order';
 
 
 
 export function Home() {
+    const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
+    const [orders, setOrders] = useState<OrderProps[]>([{
+        id: '123',
+        patrimony: '123456',
+        when: '18/07/2022 às 10:00',
+        status: 'open',
+    }]);
+
     const { colors } = useTheme();
 
     return (
@@ -38,13 +49,37 @@ export function Home() {
                     <Filter
                         type="open"
                         title="em andamento"
+                        onPress={() => setStatusSelected('open')}
+                        isActive={statusSelected === 'open'}
                     />
 
                     <Filter
                         type="closed"
                         title="finalizado"
+                        onPress={() => setStatusSelected('closed')}
+                        isActive={statusSelected === 'closed'}
                     />
                 </HStack>
+
+                <FlatList
+                    data={orders}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <Order data={item} />}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ paddingBottom: 100 }}
+                    ListEmptyComponent={() => (
+                        <Center>
+                            <ChatTeardropText color={colors.gray[300]} size={40} />
+                            <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                                Você ainda não possui {'\n'}
+                                solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+                            </Text>
+                        </Center>
+                    )}
+
+                />
+
+                <Button title="Nova solicitação" />
             </VStack>
 
 
